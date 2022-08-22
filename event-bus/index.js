ab92a4f650751000aab8466200ba63a289079e7e
Sujan Tamang <everestgaming3@gmail.com>
2022-08-22 08:11:00 +0545

    feat: add error handler to event bus APIs

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,15 @@ app.use(express.json());
 app.post("/api/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:8000/api/events", event);
-  axios.post("http://localhost:8001/api/events", event);
-  // axios.post("http://localhost:8002/api/events", event);
+  axios.post("http://localhost:8000/api/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:8001/api/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:8002/api/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
   res.status(200).json({ status: "OK" });
 });
